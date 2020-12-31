fce36617e6adb3dba42ce70b826b71eab148a714
KushiyamaRaihaanjun <kushiyama@net.lab.uec.ac.jp>
2020-12-31 17:22:34 +0900

    3回目,トポロジー，攻撃ノードセット後

--- program2/cp_and_clean.sh
+++ program2/cp_and_clean.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
-cp PDR-0-*.txt ../../grad_2020/figs/.
-cp PDR-1-*.txt ../../grad_2020/figs/.
-cp PDR-2-*.txt ../../grad_2020/figs/.
-cp PDR-3-*.txt ../../grad_2020/figs/.
-
-cp DETECT-2-*.txt ../../grad_2020/figs/.
-cp DETECT-3-*.txt ../../grad_2020/figs/.
-
-rm PDR-0-*.txt
-rm PDR-1-*.txt
-rm PDR-2-*.txt
-rm PDR-3-*.txt 
-rm DETECT-2-*.txt
-rm DETECT-3-*.txt
+cp PDR-*.txt ../../grad_2020/figs/.
+cp DETECT-*.txt ../../grad_2020/figs/.
+rm PDR-*.txt
+rm DETECT-*.txt
