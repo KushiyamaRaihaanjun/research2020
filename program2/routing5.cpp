6a5ce100e3063fba5544d7d6cb7feb799e927bf2
KushiyamaRaihaanjun <kushiyama@net.lab.uec.ac.jp>
2021-01-12 18:34:54 +0900

    exp30:packet_step=100

--- program2/routing5.cpp
+++ program2/routing5.cpp
@@ -340,7 +340,7 @@ void caliculate_and_set_dtv(ONode on[], int node_num_from, int node_num_to) //, 
     }
     else
     {
-        //インタラクションがない場合は閾値
+        //インタラクションがない場合は閾値より少し高めの値
         on[node_num_to].dtv[node_num_from] = 0.6;
     } //ここで返すか返さないか
     //return (double)(n[node_num].alpha / all_val);
